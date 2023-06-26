2309a53f51546fe3199d455f33ba8179e21222a0
2chloee <2chloeee@gmail.com>
2023-06-26 10:21:16 -0700

    Initial Commit

---
+++ makingAccount/makingAccountApp.swift
@@ -0,0 +1,17 @@
+//
+//  makingAccountApp.swift
+//  makingAccount
+//
+//  Created by Chloe Low on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct makingAccountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
